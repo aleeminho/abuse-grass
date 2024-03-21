86d362bb8e58999d91fa7b53c86b5dc78cde8fcc
alee <a.fikri2411@gmail.com>
2024-03-21 20:18:36 +0700

    perubahan kecil

---
+++ awikwok.js
@@ -0,0 +1,20 @@
+const json = require('./anjay.json')
+
+const asu = []
+
+for (each of json) {
+    if (each.protocol === 'socks5') {
+        asu.push(each)
+    }
+}
+
+const template = []
+
+for (each of asu) {
+    let username = each.username === null ? '' : username
+    let password = each.password === null ? '' : password
+    template.push(`${username}:${password}@${each.host}:${each.port}`)
+}
+
+console.log(template)
+
