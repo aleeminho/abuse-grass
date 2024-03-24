1f4d9c8aee42e52caff0f378ef5d6989135e4f31
alee <a.fikri2411@gmail.com>
2024-03-24 21:25:25 +0700

    ubah proxy riil

--- 1user_id.py
+++ 1user_id.py
@@ -74,7 +74,7 @@ async def connect_to_wss(socks5_proxy, user_id):
 
 async def main():
     #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
-    _user_id = '90ba39ae-1d6b-4eca-a876-46c89fb1f6e1'
+    _user_id = '24991439-f5a0-4ffa-875a-abc6ad82b63a'
     #put the proxy in a file in the format socks5://username:password@ip:port or socks5://ip:port
     with open('proxy_list(for1).txt', 'r') as file:
         socks5_proxy_list = file.read().splitlines()
